2a39cb054c39857b614112355739f1b7f7e36836
Mexic1 <cmorar100@yahoo.ro>
2024-10-10 23:53:41 +0300

    ex2-facut acasa

--- src/ex2/EX2.java
+++ src/ex2/EX2.java
@@ -1,7 +1,7 @@
 package ex2;
 
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
+//import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
